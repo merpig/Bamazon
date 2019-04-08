1c5019fd117a595ee00a3924902718b494e09550
Sasha Peters <peteram1@sewanee.edu>
2019-04-08 14:41:14 -0500

    sql files added to initialize database

---
+++ seed.sql
@@ -0,0 +1,17 @@
+INSERT INTO products(product_name,department_name,price,stock_quantity)
+VALUES ('Adidas Sambas','Sports',40.00,10),
+	('iPhone Case','Accessories',50.99,100),
+    ('Razor Mouse','Electronics',20.50,5),
+    ('Bose Speakers','Electronics',100.00,1),
+    ('Oak Chair','Furniture',20.00,10),
+    ('Plasma TV','Electronics',79.99,5),
+    ('Samsung Monitor','Electronics',89.99,5),
+    ('Lord of the Rings: The Fellowship of the Ring','Books',10.00,10),
+    ('Lord of the Rings: The Two Towers','Books',10.00,10),
+    ('Lord of the Rings: The Return of the King','Books',10.00,10)
+;
+
+INSERT INTO departments(department_name,over_head_costs)
+VALUES ('Sports',10000.00),
+		('Electronics',20000.00),
+        ('Accessories',30000.00);
